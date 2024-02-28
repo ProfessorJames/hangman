54600004e2b1822400ab0d36a78d8f3c38141989
Darren Tuohy <tuohy.darren@gmail.com>
2024-02-28 14:26:33 +0000

    Add function to check if guessed letter is in word

--- run.py
+++ run.py
@@ -29,9 +29,17 @@ def select_random_word():
     return word[0].lower()
 
 def display_logo():
+    """
+    This function prints the logo stored in the variable `logo`. 
+    """
     print(logo)
 
 def display_welcome_message():
+    """
+    This function prints a welcome message for the Hangman game, providing players
+    with instructions on how to play. It explains the objective of the game, which
+    is to guess letters to uncover the secret word before the hangman is fully drawn.
+    """
     print("""
     Welcome to Hangman!!!
     Guess letters to uncover the secret word.
@@ -47,11 +55,21 @@ def get_letter_input():
     characters), an error message is displayed, and the user is prompted again.
     """
     while True:
-        user_input = input("Please eneter a letter: ").lower()
+        user_input = input("Please enter a letter: ").lower()
         if len(user_input) == 1 and user_input.isalpha():
             return(user_input) 
         else:
             print("Please enter only one letter.")
+
+def check_guessed_letter_in_word(letter, word, display):
+    """
+    """
+    for i in range(len(word)):
+        if word[i] == letter:
+            display[i] = letter
+    print(display)
+    return display
+
 
 def play_game():
     display_logo()
@@ -61,15 +79,16 @@ def play_game():
     print(word)
     word_length = len(word)
 
-    display = ''
+    display = ['']
     for _ in range(word_length):
         display += '_'
 
     print(display)
 
     # Create function to get user input, validate user input i.e. only one letter, not a digit, keep asking user to enter a letter until letter is inputted.
-    get_letter_input()
+    guess = get_letter_input()
 
-    # check if letter is in wor
+    # create a function to check letter guessed bu user is in the word and update the display
+    check_guessed_letter_in_word(guess, word, display)
 
 play_game()
