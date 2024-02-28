7f7b5f65badf67e6e5a90f772cb398c5cf547abc
Darren Tuohy <tuohy.darren@gmail.com>
2024-02-28 12:10:18 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+import random
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('hangman-pp3')
+
+# possible_words = SHEET.worksheet('words')
+
+
+# select word
+words = possible_words.get_all_values()
+print(words)
+
+word = random.choice(words)
+print(word[0].lower())
+
+def select_random_word():
