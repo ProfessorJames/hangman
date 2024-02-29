0b89e0c481dff9b3f6971f07c202a20e9cd948f7
Darren Tuohy <tuohy.darren@gmail.com>
2024-02-29 13:35:10 +0000

    update hangman art and welcome messages

--- run.py
+++ run.py
@@ -36,16 +36,22 @@ def display_logo():
 
 def display_welcome_message():
     """
-This function prints a welcome message for the Hangman game, providing players
-with instructions on how to play. It explains the objective of the game, which
-is to guess letters to uncover the secret word before the hangman is fully drawn.
+    This function prints a welcome message for the Hangman game, providing players
+    with instructions on how to play. It explains the objective of the game, which
+    is to guess letters to uncover the secret word before the hangman is fully drawn.
     """
     print("""
-Welcome to Hangman!!!
-Guess letters to uncover the secret word.
-Try to guess the word before the hangman is fully drawn.
-Win by guessing the word correctly, or lose if the hangman is completed.
-    """)
+=====================================================
+|                 Welcome to Hangman!               |
+=====================================================
+|      Guess one letter at a time to reveal         |    
+|      the secret word.                             |                                        
+|      Try to guess the word before the hangman     |
+|      is fully drawn.                              |             
+|      Win by guessing the word correctly, or lose  |
+|      if the hangman is completed.                 |
+=====================================================
+""")
 
 def get_letter_input(letters_guessed):
     """
@@ -79,7 +85,7 @@ def play_game():
     display_welcome_message()
 
 # print word while developing. Delete before deployment.
-    print(word)
+    # print(word)
 
 # display a blank space for each letter in secret word
     display = []
